200b6892e3ca5977f04fa90c0209ba7dbeb9b9f3
lumucanada <38508372+lumucanada@users.noreply.github.com>
2020-12-20 01:56:30 -0500

    Add files via upload

--- output/final.R
+++ output/final.R
@@ -7,8 +7,8 @@ library(scales)
 library(kableExtra)
 
 
-survey_data <- read.csv("C:/Users/lumuc/Desktop/final 304/cleaned_data.csv") #read in the data 
-survey_data_2 <- read.csv("C:/Users/lumuc/Desktop/final 304/cleaned_data_2.csv") #read in the data 
+survey_data <- read.csv("C:/Users/lumuc/Desktop/final 304/cleaned_data.csv")   #read in the data 
+survey_data_2 <- read.csv("C:/Users/lumuc/Desktop/final 304/cleaned_data_2.csv")  #read in the data 
 
 survey_data <- survey_data %>% #Select variables of interest 
   select(q20,
@@ -18,6 +18,7 @@ survey_data <- survey_data %>% #Select variables of interest
          q2,
          q4)
 
+# logistic regression for model 1
 my_logit <- glm(
   q11 ~ q13 + q20 + q3 + q2 + q4,
   data = survey_data, family = "binomial"
@@ -32,10 +33,13 @@ survey_data_2 <- survey_data_2 %>% #Select variables of interest
          q2,
          q4)
 
+ # logistic regression for model 2
 my_logit_2 <- glm(
   q11 ~ q13 + q20 + q3 + q2 + q4,
   data = survey_data_2, family = "binomial"
 )
+
+# get the summary for both the model 1 and model 2.
 summary(my_logit)
 summary(my_logit_2)
 
